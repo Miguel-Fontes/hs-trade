2d9facebfd01df0ed789860ed37b7fe62f563108
Miguel Fontes <miguel_m_fontes@hotmail.com>
2018-01-14 19:17:37 -0200

    Implementação inicial de funcionalidade de busca de Ordens

--- src/Bitcointrade/Order.hs
+++ src/Bitcointrade/Order.hs
@@ -1,39 +1,53 @@
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
+
 module Bitcointrade.Order where
 
-import Network.HTTP.Conduit
-import Data.Aeson
-import Control.Monad
-import GHC.Generics
+import           Control.Monad
+import           Data.Aeson
+import           Data.Foldable        (asum)
+import           GHC.Generics
+import           Network.HTTP.Conduit
 
-data Message = Message { message :: String
-                       , responseData :: Entries 
-                       } deriving (Show)
+data Message = Message
+  { message      :: String
+  , responseData :: Entries
+  } deriving (Show)
 
 instance FromJSON Message where
-    parseJSON (Object v) = 
-        Message <$> v .:? "message" .!= "null"
-                <*> v .: "data"
-    
-    parseJSON _ = mzero
+  parseJSON (Object v) = 
+    Message <$> v .:? "message" .!= "null" 
+            <*> v .: "data"
+  
+  parseJSON _ = mzero
 
-data Entries = Bids { bids :: [Order]
-                    } 
-             | Asks { asks :: [Order]
-                    } deriving (Show, Generic, ToJSON, FromJSON) 
+data Entries = Entries
+  { bids :: [Order]
+  , asks :: [Order]
+  } deriving (Show, Generic)
 
-data Order = Order { unit_price :: Float
-                   , code :: String
-                   , amount :: Float 
-                   } deriving (Show, Generic, ToJSON, FromJSON)
+instance FromJSON Entries where
+  parseJSON (Object v) = Entries <$> v .: "bids" <*> v .: "asks"
+
+data Order = Order
+  { unit_price :: Float
+  , code       :: String
+  , amount     :: Float
+  } deriving (Show, Generic)
+
+instance FromJSON Order where
+  parseJSON (Object v) =
+    Order <$> v .: "unit_price" <*> 
+    v .: "code" <*> 
+    v .: "amount"
+  
+  parseJSON _ = mzero
 
 getOrders :: IO (Either String Entries)
 getOrders = do
-    response <- simpleHttp "https://api.bitcointrade.com.br/v1/public/BTC/orders"
-
-    print response
-
-    return $ case (eitherDecode response :: Either String Message) of
-        Left err -> Left err
-        Right (Message _ orders) -> Right orders
+  response <- simpleHttp "https://api.bitcointrade.com.br/v1/public/BTC/orders"
+  
+  return $
+    case (eitherDecode response :: Either String Message) of
+      Left err                 -> Left err
+      Right (Message _ orders) -> Right orders
--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,21 @@
 module Main where
 
-import Bitcointrade.Ticker
+import           Bitcointrade.Order
+import           Bitcointrade.Ticker
 
-import System.Exit
+import           System.Exit
 
 main :: IO ()
 main = do
   tickerResponse <- getTicker
-
   case tickerResponse of
     Left err -> print err
-    Right t -> print t
-
-  print "okay"
+    Right t  -> print t
+  
+  ordersResponse <- getOrders
+  case ordersResponse of
+    Left err -> print err
+    Right orders -> do
+      print ("Number of asks: " ++ (show . length . asks $ orders))
+      print ("Number of bids: " ++ (show . length . bids $ orders))
+  
